0fce02be7eef9ad9c9d4a0c4972f42ec972ecc23
ilia <115416335+Iliathebest@users.noreply.github.com>
2022-11-17 20:26:10 +0300

    41

---
+++ 41/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через   , ");
+int size= Convert.ToInt32(Console.ReadLine());
+
+ int sum = 0;       
+
+     int[] array = GetArray(size);
+      int[] GetArray(int size1)
+        {
+           int[] array = new int[size];
+            for (int i = 0; i < size1; i++)
+            {
+               array[i] =   size  ;
+                   if(array[i]>=0)
+                  {
+                   sum++;
+                  }
+            } return array;
+        } 
+            Console.WriteLine(string.Join(",",array)); 
+             Console.WriteLine("Сумма чисел больше 0 равна"+ sum);
